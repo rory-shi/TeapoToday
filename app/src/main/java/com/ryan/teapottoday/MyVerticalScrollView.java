5710907303198609b32625f02272adfec3bee4e3
Ryan <rory940311@gmail.com>
2016-05-06 15:00:45 +0800

    通过LruCache和DiskLruCache新添了网页图片加载的三级缓存

---
+++ app/src/main/java/com/ryan/teapottoday/MyVerticalScrollView.java
@@ -0,0 +1,82 @@
+package com.ryan.teapottoday;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+/**
+ * Created by rory9 on 2016/4/26.
+ */
+public class MyVerticalScrollView extends LinearLayout {
+    private int mLastXIntercept = 0;
+    private int mLastYIntercept = 0;
+    private int mLastX = 0;
+    private int mLastY = 0;
+
+    public MyVerticalScrollView(Context context) {
+        super(context);
+    }
+
+    public MyVerticalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercepted = false;
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                intercepted = false;
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                int deltaX = x - mLastXIntercept;
+                int deltaY = y - mLastYIntercept;
+                if (Math.abs(deltaY) > Math.abs(deltaX)) {
+                    intercepted =  true;
+                } else {
+                    intercepted = false;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                intercepted = false;
+                break;
+            }
+            default:
+                break;
+        }
+        mLastXIntercept = x;
+        mLastYIntercept = y;
+
+        return intercepted;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                scrollBy(0 ,-deltaY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        mLastX = x;
+        mLastY = y;
+        return true;
+    }
+}
