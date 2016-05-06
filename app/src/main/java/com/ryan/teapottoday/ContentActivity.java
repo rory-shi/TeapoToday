4f6e476370c42d243dd1cb2e2e68ca5a150cf39f
Ryan <rory940311@gmail.com>
2016-05-06 15:02:56 +0800

    通过LruCache和DiskLruCache新添了网页图片加载的三级缓存

--- app/src/main/java/com/ryan/teapottoday/ContentActivity.java
+++ app/src/main/java/com/ryan/teapottoday/ContentActivity.java
@@ -2,27 +2,28 @@ package com.ryan.teapottoday;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.v4.view.ViewPager;
-import android.view.MotionEvent;
+import android.widget.ImageView;
 
 /**
  * Created by rory9 on 2016/4/12.
  */
 public class ContentActivity extends Activity{
 
-    private ViewPager vpContent;
+    private ImageView vpContent;
     private float mLastY;
+    private MyVerticalScrollView mScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_layout);
 
-        vpContent = (ViewPager) findViewById(R.id.vp_content);
+        mScrollView = (MyVerticalScrollView) findViewById(R.id.scroll_view_content);
+
+
     }
 
-    @Override
+    /*@Override
     public boolean onTouchEvent(MotionEvent event) {
         //return super.onTouchEvent(event);
         switch (event.getAction()) {
@@ -32,17 +33,37 @@ public class ContentActivity extends Activity{
             }
             case MotionEvent.ACTION_MOVE: {
                 float y = event.getY();
-                float alphaDelta = (y - mLastY) / 1000;
+
+                float delta = y - mLastY;
+
+                float alphaDelta = (y - mLastY) / 6000;
+
+                ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) vpContent.getLayoutParams();
+                int mHeight = vpContent.getHeight();
+                lp.height = (int) (mHeight + delta/10);
+                if (lp.height<300) {
+                    lp.height = 300;
+                } else if (lp.height>600) {
+                    lp.height = 600;
+                }
+                vpContent.setLayoutParams(lp);
+
                 float alpha = vpContent.getAlpha() + alphaDelta;
                 if (alpha > 1.0) {
                     alpha = 1.0f;
                 } else if (alpha < 0.0) {
                     alpha = 0.0f;
-                }vpContent.setAlpha(alpha);
+                }
+                vpContent.setAlpha(alpha);
+             //   vpContent.scrollBy((int)(1000*alphaDelta),(int)(1000*alphaDelta));
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                break;
             }
             default:
                 break;
         }
         return true;
-    }
+    }*/
 }
--- app/src/main/java/com/ryan/teapottoday/adapter/FirstPageRecyclerViewAdapter.java
+++ app/src/main/java/com/ryan/teapottoday/adapter/FirstPageRecyclerViewAdapter.java
@@ -2,6 +2,8 @@ package com.ryan.teapottoday.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 
 import com.ryan.teapottoday.ContentActivity;
 import com.ryan.teapottoday.R;
+import com.ryan.teapottoday.model.ImageCacheManager;
 
 import java.util.ArrayList;
 
@@ -20,22 +23,22 @@ import java.util.ArrayList;
  * Created by rory9 on 2016/4/3.
  */
 public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPageRecyclerViewAdapter.ViewHolder> {
-    private ArrayList<Integer> mDataset;
-    private String[] mDateSet = {"4月13日\n三月初七","4月12日\n三月初六","4月11日\n三月初五","4月10日\n三月初四","4月9日\n三月初三","4月8日\n三月初二","4月7日\n三月初一","4月6日\n二月廿九",};
+    private ArrayList<String> mDataset;
+    private String[] mDateSet = {"4月13日\n三月初七", "4月12日\n三月初六", "4月11日\n三月初五", "4月10日\n三月初四", "4月9日\n三月初三", "4月8日\n三月初二", "4月7日\n三月初一", "4月6日\n二月廿九",};
     public static int HELLO_ITEM_HEIGHT = 335;
     private Context mContext;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
         public CardView mCardView;
         public IMyViewHolderClicks mListener;
         private ImageView ivFavorite;
         private ImageView ivInCardView;
 
-        public ViewHolder(CardView v,IMyViewHolderClicks listener) {
+        public ViewHolder(CardView v, IMyViewHolderClicks listener) {
             super(v);
             mListener = listener;
             mCardView = v;
@@ -45,6 +48,7 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
             ivInCardView.setOnClickListener(this);
             //v.setOnClickListener(this);
         }
+
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
@@ -60,14 +64,14 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
 
         public static interface IMyViewHolderClicks {
             public void onFavImgClicked(View caller);
+
             public void onImgClicked(View caller);
         }
     }
 
 
-
     // Provide a suitable constructor (depends on the kind of dataset)
-    public FirstPageRecyclerViewAdapter(Context context,ArrayList<Integer> myDataset) {
+    public FirstPageRecyclerViewAdapter(Context context, ArrayList<String> myDataset) {
         mContext = context;
         mDataset = myDataset;
     }
@@ -75,7 +79,7 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
     // Create new views (invoked by the layout manager)
     @Override
     public FirstPageRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
+                                                                      int viewType) {
         // create a new view
         CardView cv = (CardView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.my_card_view, parent, false);
@@ -88,7 +92,7 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
 
             @Override
             public void onImgClicked(View caller) {
-                Log.e("mytag","onimg clicked");
+                Log.e("mytag", "onimg clicked");
                 Intent intent = new Intent(mContext, ContentActivity.class);
                 mContext.startActivity(intent);
             }
@@ -106,14 +110,14 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
         CardView cvTop = holder.mCardView;
         TextView tvTitle = (TextView) cvTop.findViewById(R.id.tv_title_in_cv);
         TextView tvContent = (TextView) cvTop.findViewById(R.id.tv_content_in_cv);
-        ImageView ivPot = (ImageView) cvTop.findViewById(R.id.iv_in_cv);
+        final ImageView ivPot = (ImageView) cvTop.findViewById(R.id.iv_in_cv);
         TextView tvDate = (TextView) cvTop.findViewById(R.id.tv_date_card);
         ImageView ivFav = (ImageView) cvTop.findViewById(R.id.fav_in_cv);
-        if (position==0) {
+        if (position == 0) {
 
             cvTop.getLayoutParams().height = 800;
             cvTop.setAlpha(0);
-        } else if (position ==1) {
+        } else if (position == 1) {
             cvTop.getLayoutParams().height = HELLO_ITEM_HEIGHT;
             ivPot.setVisibility(View.GONE);
             tvDate.setVisibility(View.GONE);
@@ -123,21 +127,52 @@ public class FirstPageRecyclerViewAdapter extends RecyclerView.Adapter<FirstPage
             tvTitle.setTextSize(32);
 
             tvContent.setTextSize(22);
-            tvContent.setText("农历三月初七" );
+            tvContent.setText("农历三月初七");
 
         } else {
             ivPot.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            ivPot.setImageResource(mDataset.get(position - 2));
 
-            tvDate.setText(mDateSet[position-2]);
+            //get img from net
+            setImageView(ivPot, position);
+
+            tvDate.setText(mDateSet[position - 2]);
 
         }
 
     }
 
+    private void setImageView(ImageView ivPot, int position) {
+        /*RequestQueue mQueue = Volley.newRequestQueue(mContext);
+        ImageLoader imageLoader = new ImageLoader(mQueue, new BitmapLruImageCache(10*1024*1024));
+        ImageLoader.ImageListener listener = ImageLoader.getImageListener(ivPot, R.drawable.demo, R.drawable.hee);
+        imageLoader.get("http://10.0.3.2:8080/mywebapps/" + mDataset.get(position - 2), listener, 200, 200);*/
+
+        Bitmap defaultImage = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.demo);
+        Bitmap errorImage = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.hee);
+        ImageCacheManager.loadImage(mContext, "http://10.0.3.2:8080/mywebapps/" + mDataset.get(position - 2), ivPot, defaultImage, errorImage);
+
+    }
+
+    /*private void setImageView(final ImageView ivPot,int position) {
+        RequestQueue mQueue = Volley.newRequestQueue(mContext);
+            ImageRequest imageRequest = new ImageRequest("http://10.0.3.2:8080/mywebapps/" + mDataset.get(position - 2), new Response.Listener<Bitmap>() {
+                @Override
+                public void onResponse(Bitmap response) {
+                    ivPot.setImageBitmap(response);
+                }
+            }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    ivPot.setImageResource(R.drawable.demo);
+                }
+            });
+            mQueue.add(imageRequest);
+    }*/
+
+
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size()+1;
+        return mDataset.size() + 1;
     }
 }
