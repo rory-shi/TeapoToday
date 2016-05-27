62be31e3e4b75f49ea73e3f402e201d572b5cd8e
Ryan <rory940311@gmail.com>
2016-05-27 10:27:25 +0800

    点击图片后放大.

--- app/src/main/java/com/ryan/teapottoday/adapter/GridViewImageAdapter.java
+++ app/src/main/java/com/ryan/teapottoday/adapter/GridViewImageAdapter.java
@@ -1,12 +1,19 @@
 package com.ryan.teapottoday.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -15,6 +22,7 @@ import android.widget.ImageView;
 import com.ryan.teapottoday.CollectionItem;
 import com.ryan.teapottoday.R;
 import com.ryan.teapottoday.database.MyDatabaseHelper;
+import com.ryan.teapottoday.model.ImageCacheManager;
 import com.ryan.teapottoday.model.ImageCacheUtil;
 
 import java.util.ArrayList;
@@ -28,6 +36,7 @@ public class GridViewImageAdapter extends BaseAdapter {
     private Map<Integer, Boolean> mSelectMap = new HashMap<>();
     private Context mContext;
     private ArrayList<String> urls;
+    private boolean debug = true;
 
     public GridViewImageAdapter(Context context, HashMap<Integer,Boolean> map, ArrayList<String> urls) {
         mContext = context;
@@ -52,7 +61,7 @@ public class GridViewImageAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         CollectionItem item;
         if (convertView == null) {
             item = new CollectionItem(mContext);
@@ -66,7 +75,44 @@ public class GridViewImageAdapter extends BaseAdapter {
                 Context.MODE_PRIVATE);
         String url = pref.getString("url", "");*/
 
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+                View imgEntryView = inflater.inflate(R.layout.content_image, null); // 加载自定义的布局文件
+                ImageView ivContentDetail = (ImageView) imgEntryView.findViewById(R.id.iv_content_img);
+                String url = urls.get(position);
 
+                if (debug) {
+                    Log.e("debug", url);
+                }
+                Bitmap defaultImage = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.demo);
+                Bitmap errorImage = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.hee);
+                ImageCacheManager.loadImage(mContext, url, ivContentDetail, defaultImage, errorImage);
+
+                final AlertDialog dialog = new AlertDialog.Builder(mContext).create();
+
+                //ImageView img = (ImageView) imgEntryView.findViewById(R.id.demo);
+                //imageDownloader.download("图片地址", img); // 这个是加载网络图片的，可以是自己的图片设置方法
+                dialog.setView(imgEntryView); // 自定义dialog
+
+                Window dialogWindow = dialog.getWindow();
+                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+                dialogWindow.setGravity(Gravity.CENTER);
+                lp.width = mContext.getResources().getDisplayMetrics().widthPixels; // 宽度
+                //lp.height = 500; // 高度
+                dialogWindow.setAttributes(lp);
+
+
+                dialog.show();
+                // 点击布局文件（也可以理解为点击大图）后关闭dialog，这里的dialog不需要按钮
+                imgEntryView.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View paramView) {
+                        dialog.cancel();
+                    }
+                });
+            }
+        });
 
         item.setImgUrl(urls.get(position));
         //item.setImgResId(getItem(position));
--- app/src/main/java/com/ryan/teapottoday/fragments/HistoryFragment.java
+++ app/src/main/java/com/ryan/teapottoday/fragments/HistoryFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.util.SparseBooleanArray;
@@ -14,12 +15,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
 import com.ryan.teapottoday.R;
 import com.ryan.teapottoday.adapter.GridViewImageAdapter;
 import com.ryan.teapottoday.database.MyDatabaseHelper;
+import com.ryan.teapottoday.diyView.TextViewVertical;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,6 +33,8 @@ import java.util.HashMap;
  * Created by rory9 on 2016/4/6.
  */
 public class HistoryFragment extends Fragment {
+    private TextViewVertical tv;
+    private HorizontalScrollView sv;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,7 +46,28 @@ public class HistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_history,container,false);
+        tv = (TextViewVertical) view.findViewById(R.id.tv);
+        /*sv = (HorizontalScrollView) view.findViewById(R.id.sv);
 
+        //设置接口事件接收
+        Handler handler = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                switch (msg.what) {
+                    case TextViewVertical.LAYOUT_CHANGED:
+                        sv.scrollBy(tv.getTextWidth(), 0);//滚动到最右边
+                        break;
+                }
+            }
+        };
+        tv.setHandler(handler);//将Handler绑定到TextViewVertical*/
+
+        //创建并设置字体（这里只是为了效果好看一些，但为了让网友们更容易下载，字体库并没有一同打包
+        //如果需要体验下效果的朋友可以自行在网络上搜索stxingkai.ttf并放入assets/fonts/中）
+        //Typeface face=Typeface.createFromAsset(getAssets(),"fonts/stxingkai.ttf");
+        //tv.setTypeface(face);
+
+        //设置文字内容
+        tv.setText(getString(R.string.ICH_history_01));
         return view;
     }
 
