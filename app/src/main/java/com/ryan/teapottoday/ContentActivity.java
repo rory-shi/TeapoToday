4919b1d56c4db67084ed5798511e65d6f814dae9
Ryan <rory940311@gmail.com>
2016-06-01 20:26:48 +0800

    adding Weibo Share by OnekeyShare.

--- app/src/main/java/com/ryan/teapottoday/ContentActivity.java
+++ app/src/main/java/com/ryan/teapottoday/ContentActivity.java
@@ -35,6 +35,8 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class ContentActivity extends Activity {
 
@@ -120,6 +122,14 @@ public class ContentActivity extends Activity {
         setContentView(R.layout.content_layout);
         ButterKnife.bind(this);
 
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initOneKeyShare();
+            }
+        });
+
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -146,6 +156,25 @@ public class ContentActivity extends Activity {
 
 //        myVPContentPagerAdapter = new MyVPContentPagerAdapter(this,contentDir,teapotImgsList);
 //        vpContent.setAdapter(myVPContentPagerAdapter);
+    }
+
+    private void initOneKeyShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+    // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("分享");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+       // oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+
+// 启动分享GUI
+        oks.show(this);
     }
 
     private void receiveJsonFromNetwork() {
